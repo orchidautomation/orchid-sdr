18b174fa07fdcf50bb4e943dfc0c48d457cbccd1
Brandon Guerrero <brandon@orchidautomation.com>
2026-04-28 13:32:19 -0400

    Extract default SDR Convex repository

--- packages/convex/src/index.ts
+++ packages/convex/src/index.ts
@@ -2,3 +2,4 @@ export { convexProvider } from "../../framework/src/providers.js";
 export { convexModule } from "../../framework/src/builtin-modules.js";
 export { default as trellisDefaultConvexSchema } from "./schema.js";
 export { appendAuditEvent, recordSignal, recordWorkflowCheckpoint } from "./state-runtime.js";
+export * from "./default-sdr-repository.js";
